d2d6231d8e9bacc607826bdfdd2297ed938ab3c5
kevin <kevin@tom.local>
2020-01-16 17:30:33 +0100

    Modif 2 shell

--- bashAddAll.sh
+++ bashAddAll.sh
@@ -5,6 +5,9 @@ if [ "$1" == "help" ]; then
 	echo "Paramètre 2 : Nom de la branche"
 	exit
 elif [ "$1" == "" ] | [ "$2" == "" ]; then
+	echo "Manque d'un paramètre"
+	echo "Paramètre 1 : Votre nom de commit"
+	echo "Paramètre 2 : Nom de la branche"
 	exit
 fi
 git add .
